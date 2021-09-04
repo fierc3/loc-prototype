db33d83d1b81bc3f789bd2c4f11625abcf8e71a5
Mike Pullen <mike.pullen@swissgrc.com>
2021-09-04 12:59:50 +0200

    Add basic ui, editor

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import './App.css';
+import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import { createRef } from 'react';
+import { useRef } from 'react';
+
+function App() {
+const playerName: string = "a";
+const editorRef = useRef(null);
+const mapData = require('./maps/test.json');
+
+
+const testFunction = () => {
+
+}
+
+const handleEditorDidMount = (editor:any, monaco:any)  => {
+  console.log(mapData);
+  monaco.editor.defineTheme('myTheme', {
+    base: 'vs-dark',
+    inherit: true,
+    rules: [{ background: 'FFFFFF' }],
+    colors: {
+        'editor.foreground': '#FFFFFF',
+        'editor.background': '#000000',
+        'editorCursor.foreground': '#FFFFFF',
+        'editor.lineHighlightBackground': '#000000',
+        'editorLineNumber.foreground': '#FFFFFF',
+        'editor.selectionBackground': '#000000',
+        'editor.inactiveSelectionBackground': '#000000'
+    }
+});
+  console.log(editor)
+  console.log(monaco)
+  monaco.editor.setTheme("myTheme")
+  editorRef.current = editor; 
+}
+
+const showValue = () => {
+  alert(editorRef?.current);
+}
+
+
+  return (
+    <div className="Container">
+      <div className="title">
+        <h1>Legend of the Coder</h1>
+        <h5 className="subtitle">A Serious Game Project</h5>
+      </div>
+      <div className="App">
+        <div className="Play-grid">aaa</div>
+        <div className="Dev-area">
+          <Editor
+            className="mainEditor"
+            height="80vh"
+            theme="vs-dark"
+            defaultLanguage="typescript"
+            defaultValue="/* some comment
+        test */"
+            onMount={handleEditorDidMount}
+          />
+                  <div className="buttonBar">
+                    <p  className="playText">PLAY</p>
+                  </div>
+        </div>
+      </div>
+    </div >
+  );
+}
+
+export default App;
