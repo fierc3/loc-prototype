08795ce8dbbde4fad23d6e06d2cfd5eeb9b9a7c8
Mike Pullen <mike.pullen@swissgrc.com>
2021-09-05 10:26:14 +0200

    Add basic movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,198 @@
-import './App.css';
+import "./App.css";
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
-import { useRef } from 'react';
-import MapGrid from './MapGrid';
+import { useRef } from "react";
+import MapGrid from "./MapGrid";
+import { isEntityName } from "typescript";
+import { useState } from "react";
 
 function App() {
-const playerName: string = "a";
-const editorRef = useRef(null);
-const mapData = require('./maps/test.json');
 
+  type Entity = {
+    id :number
+    type: string
+    row : number
+    col : number
+    movement: number
+    props:any
+  }
+  const playerName: string = "a";
+  const editorRef = useRef(null);
+  const mapData = require("./maps/test.json");
+  const Memory: any[] = [];
 
-const testFunction = () => {
+  const testFunction = () => {};
 
-}
+  const handleEditorDidMount = (editor: any, monaco: any) => {
+    monaco.editor.defineTheme("myTheme", {
+      base: "vs-dark",
+      inherit: true,
+      rules: [{ background: "FFFFFF" }],
+      colors: {
+        "editor.foreground": "#FFFFFF",
+        "editor.background": "#000000",
+        "editorCursor.foreground": "#FFFFFF",
+        "editor.lineHighlightBackground": "#000000",
+        "editorLineNumber.foreground": "#FFFFFF",
+        "editor.selectionBackground": "#FFFFFF",
+        "editor.inactiveSelectionBackground": "#000000",
+      },
+    });
+    monaco.editor.setTheme("myTheme");
+    editorRef.current = editor;
+  };
 
-const handleEditorDidMount = (editor:any, monaco:any)  => {
-  console.log(mapData);
-  monaco.editor.defineTheme('myTheme', {
-    base: 'vs-dark',
-    inherit: true,
-    rules: [{ background: 'FFFFFF' }],
-    colors: {
-        'editor.foreground': '#FFFFFF',
-        'editor.background': '#000000',
-        'editorCursor.foreground': '#FFFFFF',
-        'editor.lineHighlightBackground': '#000000',
-        'editorLineNumber.foreground': '#FFFFFF',
-        'editor.selectionBackground': '#000000',
-        'editor.inactiveSelectionBackground': '#000000'
+  const showValue = () => {
+    alert(editorRef?.current);
+  };
+
+  const getEntities = (type: string): any[] => {
+    var entities: any[] = [];
+    for (const [rowKey, rowValue] of Object.entries(mapData.tiles.rows)) {
+      for (const [cellKey, cellValue] of Object.entries(
+        mapData.tiles.rows[rowKey]
+      )) {
+        if(cellValue && cellValue.type === type){
+          var entity: Entity = {
+            id: cellValue.id,
+            col : +cellKey,
+            row : +rowKey,
+            type: cellValue.type,
+            movement: cellValue.movement ? cellValue.movement : 0,
+            props : cellValue
+          }
+          entities.push(entity)
+        }
+      }
     }
-});
-  console.log(editor)
-  console.log(monaco)
-  monaco.editor.setTheme("myTheme")
-  editorRef.current = editor; 
-}
+    return entities;
+  };
 
-const showValue = () => {
-  alert(editorRef?.current);
-}
+  const findEntityById = (id:number) : any => {
+    for (const [rowKey, rowValue] of Object.entries(mapData.tiles.rows)) {
+      for (const [cellKey, cellValue] of Object.entries(
+        mapData.tiles.rows[rowKey]
+      )) {
+        if(cellValue && cellValue.id === id){
+          var entity: Entity = {
+            id: cellValue.id,
+            col : +cellKey,
+            row : +rowKey,
+            type: cellValue.type,
+            movement: cellValue.movement ? cellValue.movement : 0,
+            props : cellValue
+          }
+          return entity;
+        }
+      }
+    }
+  }
 
+
+  const getMonsters = ()  : Entity[] => {
+    return getEntities('monster');
+  }
+
+  const getRocks = () : Entity[]=> {
+    return getEntities('rock')
+  }
+
+  const findQueen = () : Entity => {
+    return getEntities('queen')[0]
+  }
+
+  const getHero = ()  : Entity => {
+    return getEntities('hero')[0]
+  }
+
+  const isInbounds = (x: number, y:number) : boolean => {
+    return (mapData.defaults.xsize > x && x >= 0) && (mapData.defaults.ysize > y && y >= 0)
+  }
+
+
+  //TODO(mike): FIX!!!!
+  const isBlocked = (x: number, y:number) : boolean => {
+    return isInbounds(x,y) && mapData.tiles.rows[y+""][""+x]?.types !== undefined
+  } 
+
+  const clearTile = (x: number, y:number) => {
+    mapData.tiles.rows[y+""][""+x] = null;
+  }
+
+  const setTile = (entity : Entity) =>{
+    mapData.tiles.rows[entity.row+""][""+entity.col] = entity.props;
+  }
+
+  const updateTile = (entity: Entity, oldX: number, oldY : number) => {
+    clearTile(oldX, oldY)
+    setTile(entity)
+  }
+
+  const moveUp = (entity: Entity) => {
+    var oldX = entity.col;
+    var oldY = entity.row;
+    entity.row= entity.row - entity.movement;
+    if(!isInbounds(entity.col,entity.row) ||isBlocked(entity.col,entity.row) ){
+      console.log(entity.type + " can't go up");
+      return;
+    }
+    updateTile(entity, oldX,oldY);
+  }
+
+  const moveDown = (entity: Entity) => {
+    var oldX = entity.col;
+    var oldY = entity.row;
+    entity.row=  oldY + 1;
+    if(!isInbounds(entity.col,entity.row) ||isBlocked(entity.col,entity.row) ){
+      console.log(entity.type + " can't go down");
+      return;
+    }
+    updateTile(entity, oldX,oldY);
+  }
+
+  const moveLeft = (entity:Entity) => {
+    var oldX = entity.col;
+    var oldY = entity.row;
+    entity.col = oldX - entity.movement;
+    if(!isInbounds(entity.col,entity.row) ||isBlocked(entity.col,entity.row) ){
+      console.log(entity.type + " can't go left");
+      return;
+    }
+    updateTile(entity, oldX,oldY);
+  }
+
+  const moveRight = (entity: Entity) => {
+    var oldX = entity.col;
+    var oldY = entity.row;
+    console.log(entity);
+    entity.col = oldX + entity.movement;
+    if(!isInbounds(entity.col,entity.row) ||isBlocked(entity.col,entity.row) ){
+      console.log(entity.type + " can't go right");
+      return;
+    }
+    updateTile(entity, oldX,oldY);
+  }
+
+
+  const play = () => {
+    turn();
+  }
+
+
+  const [reload, setReload] = useState(false);
+
+  const turn = () => {
+    //TODO: Calculate Movement Speed for order of execution
+    heroTurn();
+    setReload(reload ? false : true) //jesus christ this is bad
+  }
+
+  const heroTurn = () => {
+    console.log("It's the heroes turn!")
+    var heroCode = editorRef!.current!.getValue();
+    var re = /mapData/gi;
+    eval(heroCode.replace(re, 'no cheeto my mateo'));
+
+  }
 
   return (
     <div className="Container">
@@ -47,8 +201,13 @@ const showValue = () => {
         <h5 className="subtitle">A Serious Game Project</h5>
       </div>
       <div className="App">
-        <div className="Play-grid">
+        <div className="Play-grid"> 
+          {reload &&
           <MapGrid mapData={mapData}></MapGrid>
+          } 
+          {!reload &&
+          <MapGrid mapData={mapData}></MapGrid>
+          }
         </div>
         <div className="Dev-area">
           <Editor
@@ -60,12 +219,12 @@ const showValue = () => {
         test */"
             onMount={handleEditorDidMount}
           />
-                  <div className="buttonBar">
-                    <p  className="playText">PLAY</p>
-                  </div>
+          <div className="buttonBar" onClick={play}>
+            <p className="playText">PLAY</p>
+          </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
 
--- src/MapGrid.tsx
+++ src/MapGrid.tsx
@@ -3,19 +3,14 @@
 const MapGrid = (props: any) =>{
 
     const getRows = () => {
-        console.log(props)
-        console.log(props)
         var rows:any = []
         for (var rowIndex = 0; rowIndex < props.mapData.defaults.ysize; rowIndex++) {
             var row:any = [];
 
             for(var colIndex = 0; colIndex < props.mapData.defaults.xsize; colIndex++ ){
                 var current = props.mapData.tiles.rows[rowIndex+""][colIndex];
-                console.log(current);
                 row.push(<div className={"cell " + current?.type } key={colIndex+rowIndex}></div>)
-               console.log(colIndex)
             }
-            console.log("row")
             rows.push(<div className="row" key={rowIndex+row}>{row}</div>);
         }
         return rows
