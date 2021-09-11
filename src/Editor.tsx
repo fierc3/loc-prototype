b9dbccf44faaf90d5f2d1447b6e81eb811a27cff
Mike Pullen <mike.pullen@swissgrc.com>
2021-09-11 20:11:21 +0200

    Add map editor

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,12 +1,93 @@
+import { useEffect, useRef, useState } from "react";
+import MapGrid from "./MapGrid";
 
 
 const Editor = (props: any) =>{
+
+    const [reload, setReload] = useState(false);
+    const [mapData,setMapData] = useState(require("./maps/2.json")); //use lvl 2 as template
+    const playConsole:any = useRef(null);
+    const [selectedType, setSelectedType] = useState<string|null>(null);
+
+    const generate = () => {
+          if(playConsole){
+            (playConsole.current as HTMLParagraphElement).innerHTML= (JSON && JSON.stringify ? JSON.stringify(mapData) : mapData) + '<br />';
+          }
+      };
+
+    useEffect(() => {
+        if(reload){
+          setReload( r => !r);
+        }
+      }, [reload])
+
+      useEffect(() => {
+        //setupLogger();
+      }, [playConsole]);
+
+      const samples = {
+        queen: { id: 1, type: "queen" },
+        rock: { id: 2, type: "rock" },
+        monster: {
+          id: 11,
+          type: "monster",
+          movement: 1,
+          hp: 2,
+          initiative: 3,
+          xp: 100,
+        },
+        water: { id: 3, type: "water" },
+        hero: { id: 666, type: "hero", movement: 1, hp: 10, initiative: 2 },
+      };
+
+
+
+      const click = (e) => {
+          var coordinates = e.target.classList[0];
+          const col = coordinates.split("-")[0];
+          const row = coordinates.split("-")[1];
+          
+        if(!selectedType){
+            mapData.tiles.rows[row+""][col+""] = undefined;
+            setMapData(mapData);
+            setReload(true);
+            return;
+        }
+        mapData.tiles.rows[row+""][col+""] = samples[selectedType];
+        setMapData(mapData);
+        setReload(true);
+      }
 
     return (
         <>
         <div className="title">
             EDITOR
         </div >
+        <div className="App">
+        <div className="Play-grid"> 
+          {mapData && !reload?
+          <MapGrid mapData={mapData} editorClick={(e) => click(e)}></MapGrid>
+          :<MapGrid mapData={mapData} editorClick={(e) => click(e)}></MapGrid>
+          } 
+          <div className="outputs">
+            <p className="console" ref={playConsole}></p>
+          </div>
+          
+        </div>
+        <div className="Dev-area">
+            <p className="console">{selectedType}</p>
+            <div className="cell hero" onClick={() => setSelectedType("hero")}></div>
+            <div className="cell water"  onClick={() => setSelectedType("water")}></div>
+            <div className="cell rock"  onClick={() => setSelectedType("rock")}></div>
+            <div className="cell queen"  onClick={() => setSelectedType("queen")}></div>
+            <div className="cell monster"  onClick={() => setSelectedType("monster")}></div>
+            <div className="cell bg-tile"  onClick={() => setSelectedType(null)}></div>
+          <div className="buttonBar" onClick={generate}>
+            <p unselectable="on" className={'playText'}>GENERATE</p>
+          </div>
+         
+        </div>
+      </div>
         </>
       );
 }
--- src/MapGrid.tsx
+++ src/MapGrid.tsx
@@ -1,27 +1,41 @@
+const MapGrid = ({mapData, editorClick}) => {
+  const getRows = () => {
+    var rows: any = [];
+    for (
+      var rowIndex = 0;
+      rowIndex <mapData.defaults.ysize;
+      rowIndex++
+    ) {
+      var row: any = [];
 
-
-const MapGrid = (props: any) =>{
-
-    const getRows = () => {
-        var rows:any = []
-        for (var rowIndex = 0; rowIndex < props.mapData.defaults.ysize; rowIndex++) {
-            var row:any = [];
-
-            for(var colIndex = 0; colIndex < props.mapData.defaults.xsize; colIndex++ ){
-                var current = props.mapData.tiles.rows[rowIndex+""][colIndex];
-                row.push(<div className={"cell " + (current ? current.type : 'bg-tile') + (current?.dmg ? ' dmg' : '')  } key={colIndex+rowIndex}></div>)
+      for (
+        var colIndex = 0;
+        colIndex < mapData.defaults.xsize;
+        colIndex++
+      ) {
+        var current =mapData.tiles.rows[rowIndex + ""][colIndex];
+        row.push(
+          <div
+            className={
+              colIndex+"-"+rowIndex + " cell " +
+              (current ? current.type : "bg-tile") +
+              (current?.dmg ? " dmg" : "")
             }
-            rows.push(<div className="row" key={rowIndex+row}>{row}</div>);
-        }
-        return rows
-    }
-
-
-    return (
-        <div className="mapContainer">
-            {getRows()}
-        </div >
+            key={colIndex +";"+ rowIndex}
+            onClick={(e) => editorClick(e)}
+          ></div>
+        );
+      }
+      rows.push(
+        <div className="row" key={rowIndex + row}>
+          {row}
+        </div>
       );
-}
+    }
+    return rows;
+  };
 
-export default MapGrid
+  return <div className="mapContainer">{getRows()}</div>;
+};
+
+export default MapGrid;
